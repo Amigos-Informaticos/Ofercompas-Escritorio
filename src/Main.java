00a23d1f671611643401c992362d7f331ceccd27
LuisGerardoRendon <50636600+LuisGerardoRendon@users.noreply.github.com>
2021-05-21 11:51:36 -0500

    probando bots

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import vista.MainController;
 
+//Holi
 public class Main{
     public static void main(String[] args) {
         MainController.hit("Login","Login",MainController.Sizes.MID);
